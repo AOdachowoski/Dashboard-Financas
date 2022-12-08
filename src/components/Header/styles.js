cc9c686550f8425d3ef6908f6dfe32b8d44e6675
Amanda Karoline Odachowoski <100538853+AOdachowoski@users.noreply.github.com>
2022-12-08 13:49:01 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 150px;
+    text-align: center;
+    background: #008080;
+`;
+
+export const Header = styled.h1``;
+
+export const Title = styled.div`
+    padding-top: 20px;
+    color: #fff;
+    font-weight: 500;
+`;
